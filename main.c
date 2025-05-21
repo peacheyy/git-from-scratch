aa4431ffd5fcc917e4642904627c63bf82ed67cd
Evan Peachey <evanpeachey@gmail.com>
2025-05-21 16:29:13 -0400

    added sha-1 hash algorithm and tested it on test.txt

--- main.c
+++ main.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
+
+extern int sha1digest(uint8_t *digest, char *hexdigest, const uint8_t *data, size_t databytes);
 
 struct blob {
     char file_name[100];
+    char blob_hash[41];
 };
 
-int main() {
+void generatehex(char *hexdigest, uint8_t *digest)
+{
+    int idx;
+
+    for (idx = 0; idx < 20; idx++)
+    {
+        sprintf(hexdigest + (idx * 2), "%02x", digest[idx]);
+    }
+    hexdigest[40] = '\0';
+}
+
+int main()
+{
     struct blob bl1;
     strcpy(bl1.file_name, "test.txt");
 
     FILE *file;
-    char buffer[100];
+    char buffer[1024];
     size_t bytesRead;
+    size_t totalBytes = 0;
+    uint8_t digest[20];
 
     file = fopen(bl1.file_name, "r");
     if (file == NULL)
@@ -20,13 +38,16 @@ int main() {
         return 1;
     }
 
-    while ((bytesRead = fread(buffer, 1, sizeof(buffer) - 1, file)) > 0) {
-        buffer[bytesRead] = '\0';
-
-        printf("%s", buffer);
-    }
+    bytesRead = fread(buffer, 1, sizeof(buffer), file);
+    totalBytes = bytesRead;
 
     fclose(file);
+
+    sha1digest(digest, NULL, buffer, totalBytes);
+
+    generatehex(bl1.blob_hash, digest);
+
+    printf("SHA-1 hash: %s\n", bl1.blob_hash);
 
     return 0;
 }
